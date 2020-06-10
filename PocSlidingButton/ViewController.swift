8e4244376a942e9ff9d0735b400f86a4e7ca5a5f
Phatcharaphan ananpreechakun <phat.pan@gmail.com>
2020-06-10 09:53:13 +0700

    Initial Commit

---
+++ PocSlidingButton/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PocSlidingButton
+//
+//  Created by Phatcharaphan Ananpreechakun on 10/6/20.
+//  Copyright © 2020 Phatcharaphan Ananpreechakun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
