6de2c048409573aa8f429514a1fffbca96b93264
Phatcharaphan ananpreechakun <phat.pan@gmail.com>
2020-06-10 10:18:28 +0700

    adjust layout

--- PocSlidingButton/ViewController.swift
+++ PocSlidingButton/ViewController.swift
@@ -8,13 +8,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, SlidingButtonDelegate {
+
+  @IBOutlet weak var slideToOpenButton: SlidingButton!
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+   setupMarkUse()
   }
 
+  private func setupMarkUse() {
+    slideToOpenButton.delegate = self
+    slideToOpenButton.sliderViewTopDistance = 0
+    slideToOpenButton.sliderCornerRadius = 30
+    slideToOpenButton.labelText = "Mark Use"
+    slideToOpenButton.sliderBackgroundColor = .yellow
+    slideToOpenButton.textColor = .black
+    slideToOpenButton.thumbnailColor = .black
+    slideToOpenButton.thumnailImageView.image = UIImage(named: "pin_placeholder", in: nil, compatibleWith: nil)
+  }
+
+  func slideToOpenDelegateDidFinish(_ sender: SlidingButton) {
+     print("slideToOpenDelegateDidFinish::")
+  }
 
 }
 
